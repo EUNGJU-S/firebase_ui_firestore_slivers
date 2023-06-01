21d1980396af3f21594b2792f12e0eb826591a5d
Moh Yaghoub <moh10moh78@gmail.com>
2023-06-01 12:41:52 -0700

    exported the class

--- lib/firebase_ui_firestore_slivers.dart
+++ lib/firebase_ui_firestore_slivers.dart
@@ -3,3 +3,4 @@ library firebase_ui_firestore_slivers;
 export 'firestore_sliver_list_view.dart';
 export 'firestore_sliver_grid_view.dart';
 export 'firestore_grid_view.dart';
+export 'firestore_animated_sliver_list.dart';
